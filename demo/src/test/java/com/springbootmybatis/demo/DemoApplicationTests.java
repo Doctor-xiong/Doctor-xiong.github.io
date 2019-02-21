53d66445fbeb54716d4a005b18900d9a43a0aab6
xiongjieteng <xiongjieteng@meituan.com>
2019-02-21 17:25:43 +0800

    hhhhhhhh

--- demo/src/test/java/com/springbootmybatis/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/springbootmybatis/demo/DemoApplicationTests.java
@@ -5,12 +5,18 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		int[] arr = {1,2,3,4,5};
+		List list = Arrays.asList(arr);
+		System.out.println(list.size());
 	}
 
 }
